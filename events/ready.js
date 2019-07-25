bfa91017692b7332ec99efc75bddce779e8165aa
pickysaurus <pickysaurus@googlemail.com>
2019-07-25 15:09:46 +0100

    Prevent the "bot is online" message from appearing after reconnection.

--- events/ready.js
+++ events/ready.js
@@ -6,11 +6,16 @@ const onlineEmbed = new Discord.RichEmbed()
 .setTitle('Nexus Mods Discord Bot is online.')
 .setColor(0x009933)
 
+let firstStartUp = false
+
 module.exports = async (client) => {
     console.log(`${new Date()} - Ready to serve in ${client.channels.size} channels on ${client.guilds.size} servers, for a total of ${client.users.size} users.`);
     client.user.setActivity(`the channels for ${client.config.prefix[0]}`, {type: 'Watching'});
     
     if (config.testing) return console.log('Testing mode - did not publish online message.');
+    if (firstStartUp) return
+
+    firstStartUp = true
 
     for (i=0; i < serverConfig.length; i++) {
       var curServer = serverConfig[i];
