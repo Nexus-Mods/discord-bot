eb227ed2149b4fc9624a9b0a08e6adb70374c36d
Mike Watling <mike.watling@nexusmods.com>
2019-12-05 11:12:33 +0000

    Made some additional additions to LinkedMembers to help with the transition to postgres

--- util/LinkedMembers.js
+++ util/LinkedMembers.js
@@ -2,6 +2,21 @@ const Discord = require("discord.js");
 const Enmap = require("enmap");
 const nexusAPI = require('./../nexus-discord.js');
 
+let NexusModsUsers = initalise();
+
+function initalise() {
+    //Set up the database for use.
+    if (!NexusModsUsers) {
+        NexusModsUsers = new NexusModsDiscordUsers()
+        NexusModsUsers.defer.then(() => {
+            console.log(`Database loaded with ${NexusModsUsers.count} members.`);
+            return NexusModsUsers;
+        });
+    }
+    
+    return NexusModsUsers;
+}
+
 //Set up the enmaps.
 linkedAccounts = new Enmap({
     name: "NexusModsDiscordUsers",
@@ -13,6 +28,70 @@ linkedAccounts.defer.then( () =>
   //exports.linkedAccounts.deleteall()
   console.log(`Database loaded with ${exports.linkedAccounts.count} members.`)
 );
+
+//Nexus Mods members database 
+class NexusModsDiscordUsers {
+    constructor() {
+        this.members = new Enmap({
+            name: "NexusModsDiscordUsers",
+            autoFetch: true,
+            fetchAll: true
+          });
+        this.members.defer.then( () => console.log(`Database loaded with ${exports.linkedAccounts.count} members.`) );
+    }
+
+    async findMember(query, client, message) {
+        //Pass a string with the search query, the client and the message.
+        if (typeof query !== 'string') return undefined;
+        //Attempt to find a user by Discord Tag
+        const userTag = query.indexOf("#") ? query.substring(0, query.indexOf("#")+5) : query.split(" ")[0]; //if tag has several words.
+        const taggedMember = await client.users.find(u => u.tag.toLowerCase() === userTag.toLowerCase());
+
+        //Attempt to find a user by ping.
+        const discordMember = await message.mentions.members.first() //First ping
+            || message.guild.members.find(m => m.displayName.toLowerCase() === query.toLowerCase()) //Find by display name
+            || taggedMember && message.guild.members.find(m => m.id === taggedMember.id) //
+            || message.guild.members.find(m => m.id === query.split(" ")[0]) //Find by Discord ID
+
+        //Attempt to find the user in our database
+        const nexusUser = discordMember 
+            ? this.members.get(discordMember.id) //Get from DB by Discord ID
+            : this.members.find(m => m.nexusName.toLowerCase() === args.join(" ").toLowerCase()); //Find by name.
+        if (!discordMember) discordMember = message.guild.members.has(this.members.findKey(val => val === nexusUser)) 
+            ? message.guild.members.get(this.members.findKey(val => val === nexusUser)) 
+            : undefined;
+
+        return {discord: discordMember, nexus: nexusUser};
+    }
+
+    getMemberDiscord(discordId) {
+        //We know who we're looking for based on Discord ID. 
+        if (typeof discordId !== 'string') return undefined;
+        return this.members.get(discordId);
+    }
+
+    getMemberNexus(nexusName) {
+        //Lookup by Nexus Mods user name. 
+        return this.members.find(m => m.nexusName.toLowerCase() === nexusName.toLowerCase());
+    }
+
+    updateMember(discordId, newMember) {
+        //Add a new user or update the record if they already exist. 
+        this.members.get(discordId) ? this.updateMember(discordId, newMember) : this.members.set(discordId, newMember);
+        return true;
+    }
+
+    removeMember(discordId) {
+        //Delete a member.
+        if (this.members.get(discordId)) {
+            this.members.delete(discordId);
+            return true;
+        }
+        else return false;
+        
+    }
+}
+
 
 //Classes for the linked members.
 
@@ -139,4 +218,4 @@ class LinkedMod {
     }
 }
 
-exports = {linkedAccounts, LinkedMember}
+exports = { NexusModsUsers, LinkedMember };
