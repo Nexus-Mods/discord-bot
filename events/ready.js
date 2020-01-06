42bef9b79015123b8dd813b824b6095c8a0141f1
Mike Watling <mike.watling@nexusmods.com>
2020-01-06 15:54:05 +0000

    deleted old nexus-discord.js
    Converted ready.js to postgres
    Converted nexus-news to postgres

--- events/ready.js
+++ events/ready.js
@@ -1,41 +1,38 @@
-const serverConfig = require("../serverconfig.json");
-const config = require("../config.json");
+const { deleteServer, getAllServers } = require('../api/bot-db.js');
 const Discord = require('discord.js');
-const fs = require('fs');
 const onlineEmbed = new Discord.RichEmbed()
 .setTitle('Nexus Mods Discord Bot is online.')
-.setColor(0x009933)
+.setColor(0x009933);
+const newsFeed = require('../feeds/nexus-news.js');
 
 let firstStartUp = false
 
 module.exports = async (client) => {
-    console.log(`${new Date()} - Ready to serve in ${client.channels.size} channels on ${client.guilds.size} servers, for a total of ${client.users.size} users.`);
-    client.user.setActivity(`the channels for ${client.config.prefix[0]}`, {type: 'Watching'});
-    
-    if (config.testing) return console.log('Testing mode - did not publish online message.');
-    if (firstStartUp) return
+  console.log(`${new Date()} - Ready to serve in ${client.channels.size} channels on ${client.guilds.size} servers, for a total of ${client.users.size} users.`);
+  client.user.setActivity(`the channels for ${client.config.prefix[0]}`, {type: 'Watching'});
+  
+  if (client.config.testing) return console.log('Testing mode - did not publish online message.');
+  if (firstStartUp) return
 
-    firstStartUp = true
+  firstStartUp = true;
 
-    for (i=0; i < serverConfig.length; i++) {
-      var curServer = serverConfig[i];
-      if (curServer.id && !client.guilds.find(s => s.id === curServer.id)) {
-        console.log(`${new Date()} - Deleting non-existant server: ${curServer.name}`)
-        serverConfig.splice(i,1)
-        i--
-        updateJSON(serverConfig)
-      }
-      if (curServer.nexusLogChannel) {
-        var botChannel = client.channels.find(c => c.id === curServer.nexusLogChannel);
-        onlineEmbed.setTimestamp(new Date());
-        if (botChannel) botChannel.send(onlineEmbed).catch(console.error);
-      };
+  // Start the news and feeds TODO!
+  newsFeed.run(client);
+
+  const allServers = await getAllServers();
+
+  // Post the online notice.
+  for(const server of allServers) {
+    const discordGuild = client.guilds.find(s => s.id === server.id);
+    if (!discordGuild) {
+      await deleteServer(server.id);
+      return console.log(`${new Date().toLocaleString()} - Deleting non-existant server: ${server.id}`);
+    };
+    if (server.nexus_log) {
+      const logChannel = discordGuild.channels.find(c => c.id === server.nexus_log);
+      if (!logChannel) return console.log(`${new Date().toLocaleString()} - Log channel no longer exists for: ${discordGuild.name}`)
+      onlineEmbed.setTimestamp(new Date());
+      logChannel.send(onlineEmbed).catch((err) => console.error(`${new Date().toLocaleString()} - Error posting online notice to log channel in ${discordGuild.name}\n${err}`));
     };
   };
-
-  function updateJSON(newJSON) {
-    fs.writeFile("serverconfig.json", JSON.stringify(newJSON, null, 2), function(err){
-        if (err) throw err;
-        //console.log('The "data to append" was appended to file!')
-    });
 }
