3535a93d6a49edd7883b67d75f417567c106804c
Mike Watling <mike.watling@nexusmods.com>
2020-01-15 16:28:56 +0000

    Additional gamefeed work

--- api/bot-db.js
+++ api/bot-db.js
@@ -206,25 +206,67 @@ exports.updateSavedNews = (newsArticle) => {
 // GAME FEED MANAGEMENT
 
 exports.getAllGameFeeds = () => {
-
+    return new Promise((resolve, reject) => {
+        pool.query('SELECT * FROM game_feeds', (error, result) => {
+            if (error) return reject(error);
+            resolve(result.rows);
+        });
+    });
 }
 
 exports.getGameFeed = (feedId) => {
-
+    return new Promise((resolve, reject) => {
+        pool.query('SELECT * FROM game_feeds WHERE _id = $1', [feedId], (error, result) => {
+            if (error) return reject(error);
+            resolve(result.rows);
+        });
+    });
 }
 
 exports.getGameFeedsForServer = (serverId) => {
-
+    return new Promise((resolve, reject) => {
+        pool.query('SELECT * FROM game_feeds WHERE guild = $1', [serverId], (error, result) => {
+            if (error) return reject(error);
+            resolve(result.rows);
+        });
+    });
 }
 
 exports.createGameFeed = (newFeed) => {
-
+    return new Promise(
+        (resolve, reject) => {
+        pool.query('INSERT INTO game_feeds (channel, guild, owner, domain, title, nsfw, sfw, show_new, show_updates, last_timestamp, created) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11)',
+        [newFeed.channel, newFeed.guild, newFeed.owner, newFeed.domain, newFeed.title, newFeed.nsfw, newFeed.sfw, newFeed.show_new, newFeed.show_updates, Date(0), new Date()], 
+        (error, results) => {
+            if (error) {
+                //throw error;
+                console.log(error);
+                if (error.code === "23505") return reject(`Error ${error.code} - The field ${error.constraint} is not unique.`);
+            };
+            //console.log("User inserted into the database: "+user.nexusName);
+            resolve(true);
+        })
+    });
 }
 
 exports.updateGameFeed = (feedId, newData) => {
-    
+    return new Promise(async (resolve, reject) => {
+        let errors = 0;
+        Object.keys(newData).forEach((key) => {
+            pool.query(`UPDATE game_feeds SET ${key} = $1 WHERE _id = $2`, [newUser[key], feedId], (error, results) => {
+                if (error) errors += 1;
+            });
+        });
+        if (errors > 0) resolve(false);
+        else resolve(true);
+    });
 }
 
 exports.deleteGameFeed = (feedId) => {
-
+    return new Promise((resolve, reject) => {
+        pool.query('DELETE FROM game_feeds WHERE _id = $1', [feedId], (error, result) => {
+            if (error) return reject(error);
+            resolve(result.rows);
+        });
+    });
 }
