75cacd81748feaf09916b035f6fcff649fcb7a4d
Pickysaurus <pickysaurus@googlemail.com>
2021-01-02 13:42:08 +0000

    Added error handler for whoami as a test

--- src/commands/whoami.ts
+++ src/commands/whoami.ts
@@ -13,11 +13,16 @@ const help = {
 async function run(client: Client, message: Message, args: string[], serverData: BotServer) {
     const replyChannel: (GuildChannel | DMChannel | undefined | null) = serverData && serverData.channel_bot ? message.guild?.channels.resolve(serverData.channel_bot) : message.channel;
     const discordId: string = message.author.id;
-    const userData: NexusUser = await getUserByDiscordId(discordId);
-    if (!userData) return (replyChannel as TextChannel).send('You have\'t linked your account yet. See `!nm link` for more information.').catch(() => undefined);
-    const card: MessageEmbed = await userEmbed(userData, message, client);
-    return (replyChannel as TextChannel).send(replyChannel !== message.channel ? message.author : '', card)
-        .catch((err) => (replyChannel as TextChannel).send(`Error: ${err.message}`));
+    try {
+        const userData: NexusUser = await getUserByDiscordId(discordId);
+        if (!userData) return (replyChannel as TextChannel).send('You have\'t linked your account yet. See `!nm link` for more information.').catch(() => undefined);
+        const card: MessageEmbed = await userEmbed(userData, message, client);
+        return (replyChannel as TextChannel).send(replyChannel !== message.channel ? message.author : '', card)
+            .catch((err) => (replyChannel as TextChannel).send(`Error: ${err.message}`));
+    }
+    catch(err) {
+        return (replyChannel as TextChannel).send(`An Error occured with your request: ${err.message}`);
+    }
 
 }
 
