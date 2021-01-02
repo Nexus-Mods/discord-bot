d983d25af19509192c57452e6b789cae7eadc0f2
Pickysaurus <pickysaurus@googlemail.com>
2021-01-02 14:36:59 +0000

    Set timeout to 5 seconds.

--- src/api/dbConnect.ts
+++ src/api/dbConnect.ts
@@ -9,7 +9,7 @@ const poolConfig: PoolConfig = {
     ssl: !config.testing ? {
         rejectUnauthorized: false,
     } : false,
-    statement_timeout: 10000
+    statement_timeout: 5000
 };
 
 const pool = new Pool(poolConfig);
