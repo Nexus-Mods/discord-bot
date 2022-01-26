fe6448868366f302964e7a606cf6d5fc4eec3be9
Mike Watling <mike.watling@nexusmods.com>
2022-01-26 16:13:38 +0000

    Fixed log message.

--- src/api/util.ts
+++ src/api/util.ts
@@ -1,4 +1,4 @@
 export const logMessage  = (msg: string, obj?: object, error?: boolean) => {
-    const message = `${new Date().toLocaleString()} -${msg}`;
-    error === true ? console.error(message, obj) : console.log(message, obj);
+    const message = `${new Date().toLocaleString()} - ${msg}`;
+    error === true ? console.error(message, obj || '') : console.log(message, obj || '');
 };
