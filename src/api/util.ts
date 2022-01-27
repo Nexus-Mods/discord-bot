8d4553002520cb663c19e9e1c71c6c93a6841583
Pickysaurus <pickysaurus@googlemail.com>
2022-01-27 19:32:28 +0000

    Added a catch-all error handler for interactions.

--- src/api/util.ts
+++ src/api/util.ts
@@ -1,4 +1,16 @@
+import { MessageEmbed } from "discord.js";
+
 export const logMessage  = (msg: string, obj?: object, error?: boolean) => {
     const message = `${new Date().toLocaleString()} - ${msg}`;
     error === true ? console.error(message, obj || '') : console.log(message, obj || '');
 };
+
+export const unexpectedErrorEmbed = (err: any, context: any): MessageEmbed => {
+    return new MessageEmbed()
+    .setTitle('Unexpected error')
+    .setColor('DARK_RED')
+    .setDescription('The bot encountered an unexpected error with this command. You may be able to retry after a few minutes. If this issue persists, please report it including the information below.')
+    .addField('Error Details', `\'\'\'${err}\'\'\'`.substring(0,1010))
+    .addField('Error Context', `\'\'\'${JSON.stringify(context, null, 2)}\'\'\'`.substring(0,1010))
+    .addField('Reporting the error', 'Please report this on [GitHub](https://github.com/Nexus-Mods/discord-bot/issues/) or the [Nexus Mods server](https://discord.gg/nexusmods)')
+}
--- src/events/interactionCreate.ts
+++ src/events/interactionCreate.ts
@@ -1,6 +1,7 @@
 import { CommandInteraction } from 'discord.js';
 import { ClientExt } from '../DiscordBot';
 import { DiscordInteraction } from '../types/util';
+import { unexpectedErrorEmbed } from '../api/util';
 
 async function main(client: ClientExt, interaction: CommandInteraction) {
     if (!interaction.isCommand()) return;
@@ -8,7 +9,22 @@ async function main(client: ClientExt, interaction: CommandInteraction) {
 
     const interact: DiscordInteraction = client.interactions?.get(interaction.commandName);
     if (!interact) return console.error('Invalid interaction requested', interaction);
-    else return interact.action(client, interaction);
+    else {
+        try {
+            return interact.action(client, interaction);
+        }
+        catch(err) {
+            const context = {
+                serverId: interaction.guildId,
+                serverName: interaction.guild?.name,
+                channelName: interaction.channel?.toString(),
+                requestedBy: interaction.user.tag,
+                botVersion: process.version,
+                interaction: interaction.commandName
+            }
+            return interaction.reply({ embeds: [unexpectedErrorEmbed(err, context)] });
+        }
+    } 
 }
 
 
