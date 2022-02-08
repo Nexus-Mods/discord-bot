4beee952c4ea0fcf5262df439bad08621d68a970
Pickysaurus <pickysaurus@googlemail.com>
2022-02-08 19:59:31 +0000

    Trying followup instead of reply for interaction errors.

--- src/events/interactionCreate.ts
+++ src/events/interactionCreate.ts
@@ -29,15 +29,20 @@ async function main(client: ClientExt, interaction: CommandInteraction) {
             }
             const reply = { embeds: [unexpectedErrorEmbed(err, context)], components: [], content: null };
             
-            (interaction.replied || interaction.deferred) 
-            ? interaction.editReply(reply).catch(replyError => {
-                logMessage('Error editing reply for failed interaction', {replyError, ...context}, true);
+            interaction.followUp(reply).catch(replyError => {
+                logMessage('Error following up for failed interaction', { replyError, ...context, inter: interaction}, true);
                 process.exit(1);
             })
-            : interaction.reply(reply).catch(replyError => {
-                logMessage('Error replying to failed interaction', {replyError, ...context}, true);
-                process.exit(1);
-            });
+            
+            // (interaction.replied || interaction.deferred) 
+            // ? interaction.editReply(reply).catch(replyError => {
+            //     logMessage('Error editing reply for failed interaction', {replyError, ...context}, true);
+            //     process.exit(1);
+            // })
+            // : interaction.reply(reply).catch(replyError => {
+            //     logMessage('Error replying to failed interaction', {replyError, ...context}, true);
+            //     process.exit(1);
+            // });
         });
     } 
 }
