6630cc27431c286e20addf2a8d5cc4ed60fe1da9
Pickysaurus <pickysaurus@googlemail.com>
2022-02-08 20:43:28 +0000

    Removed Followup as that's less reliable.

--- src/events/interactionCreate.ts
+++ src/events/interactionCreate.ts
@@ -29,20 +29,16 @@ async function main(client: ClientExt, interaction: CommandInteraction) {
             }
             const reply = { embeds: [unexpectedErrorEmbed(err, context)], components: [], content: null };
             
-            interaction.followUp(reply).catch(replyError => {
-                logMessage('Error following up for failed interaction', { replyError, ...context, inter: interaction}, true);
+            
+            (interaction.replied || interaction.deferred) 
+            ? interaction.editReply(reply).catch(replyError => {
+                logMessage('Error editing reply for failed interaction', {replyError, ...context, interact: interaction}, true);
                 process.exit(1);
             })
-            
-            // (interaction.replied || interaction.deferred) 
-            // ? interaction.editReply(reply).catch(replyError => {
-            //     logMessage('Error editing reply for failed interaction', {replyError, ...context}, true);
-            //     process.exit(1);
-            // })
-            // : interaction.reply(reply).catch(replyError => {
-            //     logMessage('Error replying to failed interaction', {replyError, ...context}, true);
-            //     process.exit(1);
-            // });
+            : interaction.reply(reply).catch(replyError => {
+                logMessage('Error replying to failed interaction', {replyError, ...context, interact: interaction}, true);
+                process.exit(1);
+            });
         });
     } 
 }
