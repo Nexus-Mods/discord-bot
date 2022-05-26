7f083e29701e90fefb90badd9b9e73afcb0ebe86
Mike Watling <mike.watling@nexusmods.com>
2022-05-26 14:35:39 +0100

    More error handling

--- src/feeds/GameFeedManager.ts
+++ src/feeds/GameFeedManager.ts
@@ -263,8 +263,8 @@ async function checkForGameUpdates(client: ClientExt, feed: GameFeed): Promise<v
         
     }
     catch(err) {
-        logMessage('Error processing game feed:'+feed._id , err, true);
-        const error: string = (err as string) || (err as Error)?.message;
+        logMessage('Error processing game feed:'+feed._id, true);
+        const error: string = (err as Error)?.message || (err as string);
         console.warn('Error message captured', error);
         if (error.indexOf('Nexus Mods API responded with 429.') !== -1) {
             logMessage('Failed to process game feed due to rate limiting', { name: userData.name, id: feed._id, guild: guild?.name });
