59925c200d9eac3e671ca1c345f8cbfb7ed14a3e
Mike Watling <mike.watling@nexusmods.com>
2023-01-02 16:56:53 +0000

    More config changes.

--- src/server/config.ts
+++ src/server/config.ts
@@ -9,14 +9,14 @@ import nconf from 'nconf';
 nconf.env().argv().file('../.env');
 
 const config = {
-  DISCORD_TOKEN: nconf.get('DISCORD_TOKEN') || process.env.DISCORD_TOKEN,
-  DISCORD_CLIENT_ID: nconf.get('DISCORD_CLIENT_ID') || process.env.DISCORD_CLIENT_ID,
-  DISCORD_CLIENT_SECRET: nconf.get('DISCORD_CLIENT_SECRET') || process.env.DISCORD_CLIENT_SECRET,
-  DISCORD_REDIRECT_URI: nconf.get('DISCORD_REDIRECT_URI') || process.env.DISCORD_REDIRECT_URI,
-  COOKIE_SECRET: nconf.get('COOKIE_SECRET') || process.env.COOKIE_SECRET,
-  NEXUS_REDIRECT_URI: nconf.get('NEXUS_REDIRECT_URI') || process.env.NEXUS_REDIRECT_URI,
-  NEXUS_OAUTH_ID: nconf.get('NEXUS_OAUTH_ID') || process.env.NEXUS_OAUTH_ID,
-  NEXUS_OAUTH_SECRET: nconf.get('NEXUS_OAUTH_SECRET') || process.env.NEXUS_OAUTH_SECRET
+  DISCORD_TOKEN: nconf.get('DISCORD_TOKEN') ?? process.env.DISCORD_TOKEN,
+  DISCORD_CLIENT_ID: nconf.get('DISCORD_CLIENT_ID') ?? process.env.DISCORD_CLIENT_ID,
+  DISCORD_CLIENT_SECRET: nconf.get('DISCORD_CLIENT_SECRET') ?? process.env.DISCORD_CLIENT_SECRET,
+  DISCORD_REDIRECT_URI: nconf.get('DISCORD_REDIRECT_URI') ?? process.env.DISCORD_REDIRECT_URI,
+  COOKIE_SECRET: nconf.get('COOKIE_SECRET') ?? process.env.COOKIE_SECRET,
+  NEXUS_REDIRECT_URI: nconf.get('NEXUS_REDIRECT_URI') ?? process.env.NEXUS_REDIRECT_URI,
+  NEXUS_OAUTH_ID: nconf.get('NEXUS_OAUTH_ID') ?? process.env.NEXUS_OAUTH_ID,
+  NEXUS_OAUTH_SECRET: nconf.get('NEXUS_OAUTH_SECRET') ?? process.env.NEXUS_OAUTH_SECRET
 };
 
 export default config;
