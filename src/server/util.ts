f4b9fc2dda1d29f713f9831f50552e14c6f058ac
Mike Watling <mike.watling@nexusmods.com>
2023-01-02 17:04:45 +0000

    Cut out the config file as it doesn't seem to work.

--- src/server/util.ts
+++ src/server/util.ts
@@ -5,10 +5,12 @@ export function getDiscordOAuthUrl() {
     const state = crypto?.randomUUID() || 'test';
 
     console.log('config', config);
+
+    if (!process.env.DISCORD_CLIENT_ID || !process.env.DISCORD_REDIRECT_URI) return { url: '/', state };
   
     const url = new URL('https://discord.com/api/oauth2/authorize');
-    url.searchParams.set('client_id', config.DISCORD_CLIENT_ID);
-    url.searchParams.set('redirect_uri', config.DISCORD_REDIRECT_URI);
+    url.searchParams.set('client_id', process.env.DISCORD_CLIENT_ID);
+    url.searchParams.set('redirect_uri', process.env.DISCORD_REDIRECT_URI);
     url.searchParams.set('response_type', 'code');
     url.searchParams.set('state', state);
     url.searchParams.set('scope', 'role_connections.write identify');
