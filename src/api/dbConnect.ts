5e1cd7bcdf2d9284e65560b661888b858283af0f
Pickysaurus <pickysaurus@googlemail.com>
2023-01-03 20:13:59 +0000

    More debugging.

--- src/api/dbConnect.ts
+++ src/api/dbConnect.ts
@@ -18,7 +18,7 @@ const pool = new Pool(poolConfig);
 function doQuery(query: string, values: any[], callback: (err: Error, result?: QueryResult) => void) {
     pool.connect((err: Error, client: PoolClient, release) => {
         if (err) {
-            console.error('Error acquiring client', query, { err: err.message, poolConfig });
+            console.error('Error acquiring client', query, { err: err.message, poolConfig, env: process.env });
             release();
             return callback(err);
         };
