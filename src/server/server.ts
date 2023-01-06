7046f15c5a2d1493eecdfb04d507829932f11f9b
Mike Watling <mike.watling@nexusmods.com>
2023-01-06 16:05:54 +0000

    Pass the user details over again.

--- src/server/server.ts
+++ src/server/server.ts
@@ -177,7 +177,7 @@ export class AuthSite {
             }
             // Store the tokens
             const updatedUser = existingUser ? await updateUser(discordData.id, user) : await createUser({ d_id: discordData.id, ...user } as NexusUser);
-            await this.updateDiscordMetadata(discordData.id);
+            await this.updateDiscordMetadata(discordData.id, updatedUser);
             logMessage('OAuth Account link success', { discord: discordData.name, nexusMods: user.name });
             const successUrl = '/success'
             +`?nexus=${encodeURIComponent(user.name || '')}`
@@ -229,9 +229,9 @@ export class AuthSite {
         
     }
 
-    async updateDiscordMetadata(userId: string) {
+    async updateDiscordMetadata(userId: string, user?: NexusUser) {
         let metadata = {};
-        const user: NexusUser = await getUserByDiscordId(userId);
+        if (!user) user = await getUserByDiscordId(userId);
         if (!user) throw new Error('No linked users for this Discord ID.');
         if (!user.discord_access || !user.discord_refresh || !user.discord_expires) {
             throw new Error('No Discord OAuth tokens for this user');
