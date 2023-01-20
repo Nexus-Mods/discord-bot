e618a47eba1d22bd5639a206e5e5d0c1967ca882
Mike Watling <mike.watling@nexusmods.com>
2023-01-20 11:34:27 +0000

    Returned message was too long.

--- src/interactions/test.ts
+++ src/interactions/test.ts
@@ -3,6 +3,7 @@ import { DiscordInteraction, ClientExt } from "../types/DiscordTypes";
 // import { logMessage } from "../api/util";
 import { getUserByDiscordId } from '../api/bot-db';
 import { NexusModsGQLClient } from "../api/NexusModsGQLClient";
+import { logMessage } from "../api/util";
 
 const discordInteraction: DiscordInteraction = {
     command: new SlashCommandBuilder()
@@ -24,11 +25,13 @@ async function action(client: ClientExt, baseInteraction: CommandInteraction): P
     await interaction.deferReply({ ephemeral: true });
     const discordId = interaction.user.id;
     const user = await getUserByDiscordId(discordId);
-    const initial = { access_token: user.nexus_access, refresh_token: user.nexus_refresh, expires_at: user.nexus_expires };
+    const initial = { access_token: user.nexus_access?.substring(-10), refresh_token: user.nexus_refresh, expires_at: user.nexus_expires };
     const initialString = '```'+JSON.stringify(initial, null, 2)+'```';
     const GQL = await NexusModsGQLClient.create(user);
     const updated = await GQL.getAccessToken();
+    updated.access_token = updated.access_token.substring(-10);
     const updatedString = '```'+JSON.stringify(updated, null, 2)+'```';
+    logMessage('Same tokens?', initialString == updatedString);
     try {
         return interaction.editReply(`${initialString}\n\n${updatedString}`);
     }
