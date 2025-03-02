17bbdb8a3d13af31603a65dda26cb1d255d16b83
Mike <pickysaurus@googlemail.com>
2025-03-02 17:43:04 +0000

    Added ability post tips, with autocomplete

--- src/interactions/tips-manager.ts
+++ src/interactions/tips-manager.ts
@@ -31,34 +31,6 @@ const discordInteraction: DiscordInteraction = {
         )
     )
     .addSubcommand(sc =>
-        sc.setName('create')
-        .setDescription('Add a new tip by pasting in JSON data')
-        .addStringOption(option => 
-            option.setName('code')
-            .setDescription('The short code to load this tip.')    
-            .setRequired(true)
-        )
-        .addStringOption(option => 
-            option.setName('json')
-            .setDescription('The JSON data to use, must be a single line.')
-            .setRequired(true)
-        )
-    )
-    .addSubcommand(sc =>
-        sc.setName('update')
-        .setDescription('Update an existing tip with new JSON.')
-        .addStringOption(option => 
-            option.setName('code')
-            .setDescription('The short code to load this tip.')    
-            .setRequired(true)
-        )
-        .addStringOption(option => 
-            option.setName('json')
-            .setDescription('The JSON data to use, must be a single line.')
-            .setRequired(true)
-        )
-    )
-    .addSubcommand(sc =>
         sc.setName('tojson')
         .setDescription('Show the tip as JSON')
         .addStringOption(option => 
@@ -74,7 +46,7 @@ const discordInteraction: DiscordInteraction = {
     action
 }
 
-type SubCommandType = 'tojson' | 'create' | 'update' | 'add' | 'edit';
+type SubCommandType = 'tojson' | 'add' | 'edit' | 'approve' | 'delete';
 
 async function action(client: Client, baseInteraction: CommandInteraction): Promise<any> {
     const interaction = (baseInteraction as ChatInputCommandInteraction);
@@ -87,8 +59,6 @@ async function action(client: Client, baseInteraction: CommandInteraction): Prom
         case 'add' : return addNewTip(client, interaction, tips);
         case 'edit': return editExistingTip(client, interaction, tips);
         case 'tojson': return tipJSON(client, interaction, infos);
-        // case 'create': return createTip(interaction);
-        // case 'update': return updateTip(client, interaction, infos);
         default: return interaction.editReply('Error!');
     }
 }
@@ -317,77 +287,5 @@ async function tipJSON(client: Client, interaction: ChatInputCommandInteraction,
     }
     else return interaction.editReply({ content: `\`\`\`json\n${JSON.stringify(output, null, 2)}\`\`\``, embeds: output.embeds, });
 }
-
-// async function createTip(interaction: ChatInputCommandInteraction) {
-//     const userJson: string = interaction.options.getString('json', true);
-//     const name: string = interaction.options.getString('code', true);
-
-//     const components: ActionRowBuilder<ButtonBuilder>[] = [
-//         new ActionRowBuilder<ButtonBuilder>()
-//         .addComponents(
-//             new ButtonBuilder({
-//                 label: `Add ${name}`,
-//                 style: ButtonStyle.Primary,
-//                 customId: 'confirm'
-//             }),
-//             new ButtonBuilder({
-//                 label: 'Cancel',
-//                 style: ButtonStyle.Secondary,
-//                 customId: 'cancel'
-//             })
-//         )
-//     ];
-
-//     try {
-//         const messageContent = JSON.parse(userJson);
-//         const content = messageContent.content && messageContent.content.length ? messageContent.content : null;
-//         const embeds = messageContent.embed ? [new EmbedBuilder(messageContent.embed)] : [];
-//         const message: Message = await interaction.fetchReply() as Message;
-//         await interaction.editReply({ content, embeds, components });
-//         const collector = message.createMessageComponentCollector({ componentType: ComponentType.Button, time: 30000 });
-//         collector.on('collect', async (i: ButtonInteraction) => {
-//             collector.stop(); 
-//             if (i.customId === 'confirm') {
-//                 try {
-//                     await i.deferUpdate();
-//                     const infoToStore = covertToInfoResult(name, content, messageContent.embed);
-//                     await createInfo(infoToStore);
-//                     await interaction.editReply({ content: `Info created: ${name}` });
-//                 }
-//                 catch(err) {
-//                     await interaction.editReply({ content: 'Failed to insert new tip: '+(err as Error).message })
-//                 }
-//             };
-//         })
-//         collector.on('end', () => { interaction.editReply({ components: [] }).catch(e => logMessage('Error ending collector', e, true)) });
-
-//     }
-//     catch(err) {
-//         return interaction.editReply(`Error parsing JSON: ${(err as Error).message}`);
-//     }
-// }
-
-// function covertToInfoResult(name: string, message: string|null, embed: any): InfoResult {
-//     const author: string = (embed?.footer?.text as string)?.substring(embed.footer.text.indexOf('by ') + 3) || 'Nexus Mods';
-
-//     const checkValue = (input: string|undefined|null) => !!input ? input : undefined;
-
-//     return {
-//         name,
-//         message: checkValue(message),
-//         title: checkValue(embed?.title),
-//         description: checkValue(embed?.description),
-//         url: checkValue(embed?.url),
-//         thumbnail: checkValue(embed?.thumbnail?.url),
-//         image: checkValue(embed?.image),
-//         fields: embed?.fields,
-//         approved: true,
-//         author
-//     }
-// }
-
-// async function updateTip(client: Client, interaction: CommandInteraction, infos: InfoResult[]) {
-//     return interaction.editReply('Work in progress!');    
-// }
 
 export { discordInteraction };
--- src/interactions/tips.ts
+++ src/interactions/tips.ts
@@ -1,12 +1,16 @@
 import { 
     CommandInteraction, ActionRowBuilder, SelectMenuBuilder, Client, EmbedBuilder, 
     Message, InteractionCollector, ButtonBuilder, User, SlashCommandBuilder, 
-    ChatInputCommandInteraction, ButtonStyle, SelectMenuOptionBuilder 
+    ChatInputCommandInteraction, ButtonStyle, SelectMenuOptionBuilder, 
+    AutocompleteInteraction,
+    EmbedData,
+    InteractionReplyOptions
 } from "discord.js";
 import { InfoResult, PostableInfo } from "../types/util";
 import { DiscordInteraction } from '../types/DiscordTypes';
-import { getAllInfos, displayInfo } from '../api/bot-db';
+import { getAllInfos, displayInfo, getAllTips } from '../api/bot-db';
 import { logMessage } from "../api/util";
+import { ITip } from "../api/tips";
 
 const discordInteraction: DiscordInteraction = {
     command: new SlashCommandBuilder()
@@ -15,7 +19,8 @@ const discordInteraction: DiscordInteraction = {
     .addStringOption(option =>
         option.setName('code')
         .setDescription('Quick code for known message. (Optional)')
-        .setRequired(false)    
+        .setRequired(false)
+        .setAutocomplete(true)    
     )
     .addUserOption(option =>
         option.setName('user')
@@ -25,8 +30,40 @@ const discordInteraction: DiscordInteraction = {
     .setDMPermission(true) as SlashCommandBuilder,
     public: true,
     guilds: [],
-    action
+    action,
+    autocomplete
 }
+
+class TipCache {
+    private tips : ITip[] = [];
+    private nextUpdate: number = new Date().getTime() + 300000;
+
+    constructor() {
+        getAllTips()
+        .then( t =>  {
+            this.tips = t;
+            this.setNextUpdate();
+        });
+    }
+
+    private setNextUpdate() {
+        this.nextUpdate = new Date().getTime() + 300000
+    }
+    
+    public async getApprovedTips() {
+        return this.tips.filter(t => t.approved === true);
+    }
+
+    public async getTips(): Promise<ITip[]> {
+        if (new Date().getTime() > this.nextUpdate) {
+            this.tips = await getAllTips();
+            this.setNextUpdate();
+        }
+        return this.tips;
+    }
+}
+
+let tipCache: TipCache;
 
 async function action(client: Client, baseInteraction: CommandInteraction): Promise<any> {
     const interaction = (baseInteraction as ChatInputCommandInteraction);
@@ -36,15 +73,29 @@ async function action(client: Client, baseInteraction: CommandInteraction): Prom
     const user: User | null = interaction.options.getUser('user');
 
     const data: InfoResult[] = await getAllInfos().catch(() => []);
+    if (!tipCache) tipCache = new TipCache();
+    const tips: ITip[] = await tipCache.getTips().catch(() => []);
     let content: string | null = null;
 
-    if (!data.length) return interaction.editReply('No infos available.');
+    if (!data.length && !tips.length) return interaction.editReply('No infos available.');
 
     if (!!message) {
         const selected: InfoResult|undefined = data.find(i => i.name.toLowerCase() === message.toLowerCase());
+        const tip: ITip | undefined = tips.find(t => t.prompt.toLowerCase() === message.toLowerCase());
         if (!!selected) {
             await interaction.editReply({ content: 'Info posted!', embeds: [], components: [] });
             return displaySelected(client, selected, interaction, user);
+        }
+        else if (!!tip) {
+            await interaction.editReply({ content: 'Tip posted!', embeds: [], components: [] });
+            const postable: InteractionReplyOptions = { embeds: [] };
+            if (tip.message) postable.content = tip.message;
+            if (tip.embed) {
+                const embedData = JSON.parse(tip.embed) as EmbedData;
+                const embedToShow = new EmbedBuilder(embedData);
+                postable.embeds = [ embedToShow ]
+            }
+            return interaction.followUp(postable);
         }
         else content = `No results found for ${message}`;
     }
@@ -122,6 +173,21 @@ async function displaySelected(client: Client, selected: InfoResult, interaction
     logMessage('Posting interaction', { selected: selected.name });
     const postable: PostableInfo = displayInfo(client, selected, user);
     return interaction.followUp(postable);
+}
+
+async function autocomplete(client: Client, interaction: AutocompleteInteraction) {
+    const focused = interaction.options.getFocused();
+    try {
+        const tips = await getAllTips();
+        const filtered = tips.filter(t => focused === '' || t.prompt.toLowerCase().includes(focused.toLowerCase()) || t.title.toLowerCase().includes(focused.toLowerCase()) );
+        await interaction.respond(
+            filtered.map(t => ({ name: t.title, value: t.prompt })).slice(0, 25)
+        );
+    }
+    catch(err) {
+        logMessage('Error autocompleting tips', {err}, true);
+        throw err;
+    }
 }
 
 export { discordInteraction };
