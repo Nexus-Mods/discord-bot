d9dc6bd949bc6e6fb511152d2050eee4a0685fcc
Mike Watling <mike.watling@nexusmods.com>
2025-03-12 16:03:17 +0000

    Removed columns from prod, so removing commented out keys

--- src/types/servers.ts
+++ src/types/servers.ts
@@ -3,15 +3,9 @@ import { Snowflake } from 'discord.js';
 export interface BotServer {
     id: Snowflake;
     official: boolean;
-    // channel_bot?: Snowflake;
     channel_nexus?: Snowflake;
-    // channel_log?: Snowflake;
     channel_news?: Snowflake;
     role_author?: Snowflake;
-    // role_premium?: Snowflake;
-    // role_supporter?: Snowflake;
-    // role_linked?: Snowflake;
-    // author_min_downloads?: string;
     game_filter?: string;
     server_owner: Snowflake;
 }
