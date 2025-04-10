c1a00804b20293c5fa60923610cf3cdd3b54c1f1
Mike Watling <mike.watling@nexusmods.com>
2025-04-10 14:10:54 +0100

    Try building the URL another way

--- src/api/forumAPI.ts
+++ src/api/forumAPI.ts
@@ -7,7 +7,10 @@ import { ForumTopic } from "../types/ForumWebhookTypes";
 export async function getTopic(id: number): Promise<ForumTopic> {
     const APIKEY: string | undefined = process.env.FORUM_API_KEY;
     if (!APIKEY || !APIKEY.length) throw new Error('FORUM_API_KEY is not set in the environment variables.');
-    const response = await fetch(`https://forums.nexusmods.com/api/forums/topics/${id}?key=${APIKEY}`, {
+    const params = new URLSearchParams({ key: APIKEY });
+    const forumsEndpoint = `https://forums.nexusmods.com/api/forums/topics/${id}?${params.toString()}`;
+
+    const response = await fetch(forumsEndpoint, {
         method: 'GET',
         headers: {
             'Content-Type': 'application/json',
