36f45022d961cdbf3a70b98523c8e1a7858add34
Mike Watling <mike.watling@nexusmods.com>
2025-04-10 14:16:27 +0100

    Try using axios instead of fetch

--- src/api/forumAPI.ts
+++ src/api/forumAPI.ts
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 import { ForumTopic } from "../types/ForumWebhookTypes";
+import axios from 'axios';
 
 export async function getTopic(id: number): Promise<ForumTopic> {
     const APIKEY: string | undefined = process.env.FORUM_API_KEY;
@@ -10,18 +11,25 @@ export async function getTopic(id: number): Promise<ForumTopic> {
     const params = new URLSearchParams({ key: APIKEY });
     const forumsEndpoint = `https://forums.nexusmods.com/api/forums/topics/${id}?${params.toString()}`;
 
-    const response = await fetch(forumsEndpoint, {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json',
-            'User-Agent': 'Nexus Mods Discord Bot',
-            'Accept': '*/*'
-        },
-    });
-
-    if (!response.ok) {
-        console.error('Error fetching topic:', { body: (await response.text()).substring(0, 1000) });
+    const res = await axios.get<ForumTopic>(forumsEndpoint, {});
+    if (res.status >= 200 && res.status < 300) return res.data;
+    else {
+        console.error('Error fetching topic:', { status: res.status, statusText: res.statusText });
+        throw new Error(`Error fetching topic: ${res.status} ${res.statusText}`);
     }
 
-    return await response.json();
+    // const response = await fetch(forumsEndpoint, {
+    //     method: 'GET',
+    //     headers: {
+    //         'Content-Type': 'application/json',
+    //         'User-Agent': 'Nexus Mods Discord Bot',
+    //         'Accept': '*/*'
+    //     },
+    // });
+
+    // if (!response.ok) {
+    //     console.error('Error fetching topic:', { body: (await response.text()).substring(0, 1000) });
+    // }
+
+    // return await response.json();
 }
