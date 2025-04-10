8efc4fb7d0e82fe9be77c68a2a6f4de2b971e43a
Mike Watling <mike.watling@nexusmods.com>
2025-04-10 14:23:41 +0100

    Added try/catch block

--- src/api/forumAPI.ts
+++ src/api/forumAPI.ts
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 import { ForumTopic } from "../types/ForumWebhookTypes";
-import axios from 'axios';
+import axios, { AxiosError } from 'axios';
 
 export async function getTopic(id: number): Promise<ForumTopic> {
     const APIKEY: string | undefined = process.env.FORUM_API_KEY;
@@ -11,11 +11,24 @@ export async function getTopic(id: number): Promise<ForumTopic> {
     const params = new URLSearchParams({ key: APIKEY });
     const forumsEndpoint = `https://forums.nexusmods.com/api/forums/topics/${id}?${params.toString()}`;
 
-    const res = await axios.get<ForumTopic>(forumsEndpoint, {});
-    if (res.status >= 200 && res.status < 300) return res.data;
-    else {
-        console.error('Error fetching topic:', { status: res.status, statusText: res.statusText });
-        throw new Error(`Error fetching topic: ${res.status} ${res.statusText}`);
+    try {
+        const res = await axios.get<ForumTopic>(forumsEndpoint, {
+            headers: {
+                'Content-Type': 'application/json',
+                'User-Agent': 'Nexus Mods Discord Bot',
+                'Accept': '*/*'
+            },
+            
+        });
+        if (res.status >= 200 && res.status < 300) return res.data;
+        else {
+            console.error('Error fetching topic:', { status: res.status, statusText: res.statusText, forumsEndpoint });
+            throw new Error(`Error fetching topic: ${res.status} ${res.statusText}`);
+        }
+    }
+    catch(err) {
+        const error = (err as AxiosError);
+        console.error('Error fetching topic:', { error: error.message, status: error.code, forumsEndpoint });
     }
 
     // const response = await fetch(forumsEndpoint, {
