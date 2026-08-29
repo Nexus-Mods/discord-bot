6b9f37d1e77fb57d5d343987e8cc705144e6ff1c
Mike Watling <mike.watling@nexusmods.com>
2026-08-29 19:30:47 +0100

    Re-ran codegen

--- src/api/generated/types.ts
+++ src/api/generated/types.ts
@@ -1,5 +1,10 @@
 export type Maybe<T> = T | null;
 export type InputMaybe<T> = Maybe<T>;
+export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
+export type MakeOptional<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]?: Maybe<T[SubKey]> };
+export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]: Maybe<T[SubKey]> };
+export type MakeEmpty<T extends { [key: string]: unknown }, K extends keyof T> = { [_ in K]?: never };
+export type Incremental<T> = T | { [P in keyof T]?: P extends ' $fragmentName' | '__typename' ? T[P] : never };
 /** All built-in and custom scalars, mapped to their actual values */
 export type Scalars = {
   ID: { input: string; output: string; }
@@ -9,9 +14,9 @@ export type Scalars = {
   Float: { input: number; output: number; }
   BigInt: { input: string; output: string; }
   DateTime: { input: string; output: string; }
-  ISO8601DateTime: { input: unknown; output: unknown; }
+  ISO8601DateTime: { input: any; output: any; }
   JSON: { input: unknown; output: unknown; }
-  Upload: { input: unknown; output: unknown; }
+  Upload: { input: any; output: any; }
 };
 
 export type AbstainFromModEndorsementMutationPayload = {
